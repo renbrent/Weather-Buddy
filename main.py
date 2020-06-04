8fdd3d3077ea9f6b0f6e95181782ea100e031a2e
armallama <58412994+armallama@users.noreply.github.com>
2020-06-04 15:03:07 +0900

    commit on 'v0.5'

---
+++ main.py
@@ -0,0 +1,7 @@
+import sys, requests
+import PySide2.QtCore import Qt, QUrl
+from Pyside2.QtWidgets import (QApplication, QInputDialog, QHeaderView, QHBoxLayout, QLabel, QLIneEdit,
+                               QMainWindow, QMessageBox, QPushButton, QSizePOlicy, QSlider,
+                               QStyle, QVBoxLayout, QWidget)
+from PySide2.QtMultimedia import QMediaPlayer
+from PySide2.QtGui import QIcon
